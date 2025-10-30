c423cdbba22da979ba8a599b22b8f39b6db95415
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-10-30 11:35:00 +0100

    Copy repro

--- test/ApplePlatformsIntegrationTestApp/ApplePlatformsIntegrationTests.mm
+++ test/ApplePlatformsIntegrationTestApp/ApplePlatformsIntegrationTests.mm
@@ -20,18 +20,66 @@ using namespace std;
 
 @implementation ApplePlatformsIntegrationTests
 
-- (void)testBasicRuntime {
+static string commonCode = R"(
+function generateLargeArray(type) {
+  const largeArray = [];
+  for (let i = 0; i < 5_000_000; i++) {
+    if (type === 'map') largeArray.push(new Map());
+    if (type === 'set') largeArray.push(new Set());
+    if (type === 'fun') largeArray.push(() => {});
+    if (type === 'obj') largeArray.push({});
+    if (type === 'str') largeArray.push(`${i} this is very very very long string`);
+  }
+  return largeArray;
+}
+
+const result = generateLargeArray(dataType);
+print(JSON.stringify(HermesInternal.getInstrumentedStats(), undefined, 2));
+)";
+
+- (void)testMemoryUsageMap {
   facebook::hermes::HermesRuntime::DebugFlags flags;
 
   auto runtime = facebook::hermes::makeHermesRuntime();
+  auto typedCode = "const dataType = 'map';\n" + commonCode;
 
-  string error = "It is not a bug, it is a feature!";
+  runtime->debugJavaScript(typedCode, "", flags);
+}
 
-  try {
-    runtime->debugJavaScript("throw new Error('" + error + "')", "", flags);
-  } catch (const exception &e) {
-    XCTAssertTrue(string(e.what()).find(error) != string::npos);
-  }
+- (void)testMemoryUsageSet {
+  facebook::hermes::HermesRuntime::DebugFlags flags;
+
+  auto runtime = facebook::hermes::makeHermesRuntime();
+  auto typedCode = "const dataType = 'set';\n" + commonCode;
+
+  runtime->debugJavaScript(typedCode, "", flags);
+}
+
+- (void)testMemoryUsageFunction {
+  facebook::hermes::HermesRuntime::DebugFlags flags;
+
+  auto runtime = facebook::hermes::makeHermesRuntime();
+  auto typedCode = "const dataType = 'fun';\n" + commonCode;
+
+  runtime->debugJavaScript(typedCode, "", flags);
+}
+
+- (void)testMemoryUsageObject {
+  facebook::hermes::HermesRuntime::DebugFlags flags;
+
+  auto runtime = facebook::hermes::makeHermesRuntime();
+  auto typedCode = "const dataType = 'obj';\n" + commonCode;
+
+  runtime->debugJavaScript(typedCode, "", flags);
+}
+
+- (void)testMemoryUsageString {
+  facebook::hermes::HermesRuntime::DebugFlags flags;
+
+  auto runtime = facebook::hermes::makeHermesRuntime();
+  auto typedCode = "const dataType = 'str';\n" + commonCode;
+
+  runtime->debugJavaScript(typedCode, "", flags);
 }
 
 @end
